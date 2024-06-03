9e77aedb57236c05b4b7d253ba418acddef7ba9d
zamm079 <100558930+zamm079@users.noreply.github.com>
2024-06-03 11:10:26 -0600

    2

--- dif_central.py
+++ dif_central.py
@@ -11,12 +11,24 @@ def diferencia_central_gradiente(f,x):
         f1=f(xcopy1)
         f2=f(xcopy2)
         xfinal.append((f1-f2)/(2*Dx*x[i]))
-        # print(U_fxi/(2*Dx*x[i]))
     
     return xfinal
 
 def diferencia_central_hesiana(f,x):
-    
+    Dx=0.01
+    xfinal=[]
+    for i in range(len(x)):
+        xcopy1=np.copy(x)
+        xcopy2=np.copy(x)
+        for j in range(len(x)):
+            xcopy1[j]=x[j]+Dx*x[j]
+            xcopy2[j]=x[j]-Dx*x[j]
+            fx=f(x)
+            f1=f(xcopy1)
+            f2=f(xcopy2)
+            print((f1-fx+f2)/(Dx*x[j])**2)
+            
+
 
 
 def funcion_prueba(x):
@@ -27,3 +39,4 @@ xt=np.transpose(np.array([1.0,1.0]))
 
 
 print(diferencia_central_gradiente(funcion_prueba,xt))
+diferencia_central_hesiana(funcion_prueba,xt)
